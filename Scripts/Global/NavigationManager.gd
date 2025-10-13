8ab922ad6108f865eecbb947e7b3b02919a0d5b6
Nicolas Coutinho da Silva <NicolasC@ctismad.local>
2025-10-13 13:44:57 -0300

    cenario e muitas coisas

--- Scripts/Global/NavigationManager.gd
+++ Scripts/Global/NavigationManager.gd
@@ -1,19 +1,11 @@
 extends Node
 
-const room_1 = preload("res://Scenes/Map/SR1.tscn")
-const boss_room = preload("res://Scenes/Map/SBR.tscn")
-
-
 var spawn_room
 
 func go_to_level(actual_level,destination):
-	var level_to_be_changed
+	var level_to_be_changed = load("res://Scenes/Map/"+destination+".tscn")
 	
-	match destination:
-		"room_1":
-			level_to_be_changed = room_1
-		"boss_room":
-			level_to_be_changed = boss_room
+
 	
 	if level_to_be_changed != null:
 		spawn_room = destination
