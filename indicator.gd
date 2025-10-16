5a88fc536ca6348e45289e9b331f67dccfb2728a
Miguel Pires Cardona <ctism202411276008@ctismad.local>
2025-10-16 11:00:13 -0300

    Indicador + Item message

---
+++ indicator.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var ogpos:Vector2
+@export var frequency := 1.0
+@export var amplitude := 1.0
+
+func _ready() -> void:
+	print("AA")
+	ogpos = global_position
+	
+func _process(delta: float) -> void:
+	if ogpos==null:return
+	print(ogpos)
+	global_position = ogpos + Vector2(0,sin(Engine.get_process_frames() * frequency) * amplitude)
